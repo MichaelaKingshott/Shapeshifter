4fb6b7d18e2e3d9d4b9cff9e2eeb49031cbba55d
MichaelaKingshott <kingshottm946@gmail.com>
2026-05-05 20:18:46 +0100

    p

---
+++ Assets/Scripts/Menus/EndScreen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class EndScreen : MonoBehaviour
+{
+    public void Home()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
